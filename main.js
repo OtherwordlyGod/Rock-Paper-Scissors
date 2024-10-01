d657824686acb75d33d388926ee5e9ad34b90523
OtherwordlyGod <otherwordlygod@gmail.com>
2024-10-01 13:27:27 -0400

    added comments into the code and changed some unused code

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
+//declaring variables
 let humanScore = 0;
 let computerScore = 0;
-let humanSelection;
-let computerSelection;
 
+//This function decides the computer's choice
 function getComputerChoice(){
-    let rng = Math.floor(Math.random()*3);
+    let rng = Math.floor(Math.random()*3); //This variable uses the math object to pick a random number between 0-2
 
-    if (rng === 0){
+    //this if statement converts the number into one of the choices
+    if (rng === 0){ 
         return "rock";
     } else if (rng === 1){
         return "paper";
@@ -15,9 +16,11 @@ function getComputerChoice(){
     }
 }
 
+//This function prompts the user to input their choice
 function getHumanChoice(){
-    let choice = prompt("Enter your choice")
+    let choice = prompt("Enter your choice") //This displays a popup prompt that allows the user to type their answer
 
+    //this code makes their answer case insensitive.
     if (choice.toLowerCase() === "rock"){
         return "rock";
     } else if (choice.toLowerCase() === "paper"){
@@ -29,6 +32,7 @@ function getHumanChoice(){
     }
 }
 
+//This function determines who won based on their choice and returns the answer.
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
@@ -45,16 +49,20 @@ function playRound(humanChoice, computerChoice) {
     
 }
 
-
+//This function runs the game 5 times and logs messages into the console.
 function playGame() {
 
-  let numRound = 5;
-  for (let i = 0; i < numRound; i++){
+  let numRound = 5; //this variable tells the game to run a certain amount of times.
+  
+  for (let i = 0; i < numRound; i++){ //this for loop runs the game numRound amount of times.
+
+    //these variables are declared in the for loop so that the functions give us different values from the computer and prompts the user each time.
       let humanSelection = getHumanChoice();
       let computerSelection = getComputerChoice();
       playRound(humanSelection, computerSelection);
       let result = playRound(humanSelection, computerSelection);
 
+      //this if statement logs messages into the console informing the user on the game's progress.
       if (result === "win") {
         console.log("You win!");
          humanScore++;
@@ -73,6 +81,7 @@ function playGame() {
       console.log("next round");
   }
   
+  //This if statement logs the win, loss, or tie message into the console after the for loop runs.
   if (humanScore > computerScore) {
     console.log("Congrats! You win! Refresh the page to try again.");
   } else if (computerScore > humanScore) {
