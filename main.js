668520c35858a1d53d181d6e2e1356f7036bed98
OtherwordlyGod <otherwordlygod@gmail.com>
2024-11-29 13:18:13 -0500

    refactored code to use DOM manipulation. Now has buttons and is playable in the browser

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 //declaring variables
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+
+const div = document.getElementById("container");
+const btn1 = document.createElement("button");
+const btn2 = document.createElement("button");
+const btn3 = document.createElement("button");
+
+btn1.textContent = "Rock";
+btn2.textContent = "Paper";
+btn3.textContent = "Scissors";
+
+div.appendChild(btn1);
+div.appendChild(btn2);
+div.appendChild(btn3);
+
+const player = document.getElementById("display-player");
+const resultText = document.getElementById("results");
+const message = document.getElementById("message");
 
 //This function decides the computer's choice
 function getComputerChoice(){
@@ -16,21 +34,26 @@ function getComputerChoice(){
     }
 }
 
-//This function prompts the user to input their choice
-function getHumanChoice(){
-    let choice = prompt("Enter your choice") //This displays a popup prompt that allows the user to type their answer
+    message.textContent = `Your score is: ${humanScore} The computers score is: ${computerScore}`;
 
-    //this code makes their answer case insensitive.
-    if (choice.toLowerCase() === "rock"){
-        return "rock";
-    } else if (choice.toLowerCase() === "paper"){
-        return "paper";
-    } else if (choice.toLowerCase() === "scissors"){
-        return "scissors";
-    } else {
-        alert("Please enter a valid choice of rock, paper, or scissors")
-    }
-}
+    btn1.addEventListener("click", (event) => {
+       humanChoice = "rock";
+       player.textContent = `Your choice is ${humanChoice}`
+       playRoundAndUpdate();
+    });
+
+    btn2.addEventListener("click", (event) => {
+        humanChoice = "paper";
+        player.textContent = `Your choice is ${humanChoice}`
+        playRoundAndUpdate();
+     });
+     
+    btn3.addEventListener("click", (event) => {
+        humanChoice = "scissors";
+        player.textContent = `Your choice is ${humanChoice}`
+        playRoundAndUpdate();
+    });
+
 
 //This function determines who won based on their choice and returns the answer.
 function playRound(humanChoice, computerChoice) {
@@ -49,48 +72,55 @@ function playRound(humanChoice, computerChoice) {
     
 }
 
-//This function runs the game 5 times and logs messages into the console.
-function playGame() {
+function playRoundAndUpdate() {
+    if (!humanChoice) return; 
+    disableChoiceButtons();
 
-  let numRound = 5; //this variable tells the game to run a certain amount of times.
-  
-  for (let i = 0; i < numRound; i++){ //this for loop runs the game numRound amount of times.
-
-    //these variables are declared in the for loop so that the functions give us different values from the computer and prompts the user each time.
-      let humanSelection = getHumanChoice();
-      let computerSelection = getComputerChoice();
-      playRound(humanSelection, computerSelection);
-      let result = playRound(humanSelection, computerSelection);
-
-      //this if statement logs messages into the console informing the user on the game's progress.
-      if (result === "win") {
-        console.log("You win!");
-         humanScore++;
-         console.log(`Your score is: ${humanScore}`);
-         console.log(`Your score is: ${computerScore}`);
-      } else if (result === "lose") {
-        console.log("You lose!");
+    let computerChoice = getComputerChoice();
+    let result = playRound(humanChoice, computerChoice);
+    
+    if (result === "win") {
+        humanScore++;
+        resultText.textContent = "You win this round";
+    } else if (result === "lose") {
         computerScore++;
-        console.log(`Your score is: ${humanScore}`);
-        console.log(`Your score is: ${computerScore}`);
-      } else {
-        console.log("You tied!");
-        console.log(`Your score is: ${humanScore}`);
-        console.log(`Your score is: ${computerScore}`);
-      }
-      console.log("next round");
-      //The positioning/formatting of the code above was taken inspiration from a reddit post from JScarz10. link: https://www.reddit.com/r/learnprogramming/comments/szo1kl/stuck_on_the_odin_project_rock_paper_scissors/
-  }
-  
-  //This if statement logs the win, loss, or tie message into the console after the for loop runs.
-  if (humanScore > computerScore) {
-    console.log("Congrats! You win! Refresh the page to try again.");
-  } else if (computerScore > humanScore) {
-    console.log("Sorry but you lost. Refresh the page to try again.");
-  } else {
-    console.log("WOW! You managed to tie all 5 times! The odds of that happening is 0.0041%. Refresh the page to try again.");
-  }
-  return "done";
+        resultText.textContent = "You lose this round";
+    } else {
+        resultText.textContent = "Its a draw!";
+    }
+    
+    if (humanScore === 5 || computerScore === 5) {
+        message.textContent = `Your score is: ${humanScore} The computers score is: ${computerScore}`;
+        displayResults();
+    } else {
+        message.textContent = `Your score is: ${humanScore} The computers score is: ${computerScore}`;
+        enableChoiceButtons();
+    }
+    
+};
+
+function displayResults() {
+    if (humanScore > computerScore) {
+        resultText.textContent = "You win the game! Refresh to start over";
+    } else if (humanScore < computerScore) {
+        resultText.textContent = "You lose the game! Refresh to start over";
+    } else {
+        resultText.textContent = "You tied the game! Refresh to start over";
+    }
+};
+
+
+
+// Disables choice buttons
+function disableChoiceButtons() {
+    btn1.disabled = true;
+    btn2.disabled = true;
+    btn3.disabled = true;
 }
 
-playGame();
+// Re-enable choice buttons (for the next round)
+function enableChoiceButtons() {
+    btn1.disabled = false;
+    btn2.disabled = false;
+    btn3.disabled = false;
+}
